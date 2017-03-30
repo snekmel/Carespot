0f50a1e762f9c677d20b979b13c730508d5cc252
PeterSmallenbroek <PeterSmallenbroek@users.noreply.github.com>
2017-03-30 10:22:49 +0200

    More changes

--- Carespot/Models/Beoordeling.cs
+++ Carespot/Models/Beoordeling.cs
@@ -2,5 +2,10 @@
 {
     public class Beoordeling
     {
+        public int Id { get; private set; }
+        public string Opmerking { get; private set; }
+        public int VoorGebruikersId { get; private set; }
+        public int Cijfer { get; private set; }
+        public string Reactie { get; private set; }
     }
 }
