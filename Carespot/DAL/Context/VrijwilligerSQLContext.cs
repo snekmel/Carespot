bd31d18a1ddc0a2456b8f1c38ee7b342dc44faca
Lars <larslemkens@gmail.com>
2017-04-05 12:31:32 +0200

    Bezig met vrijwilliger

---
+++ Carespot/DAL/Context/VrijwilligerSQLContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carespot.DAL.Interfaces;
+using Carespot.Models;
+
+namespace Carespot.DAL.Context
+{
+    class VrijwilligerSQLContext : IVrijwilligerContext
+    {
+        private readonly SqlConnection _con = new SqlConnection("Data Source=WIN-SRV-WEB.fhict.local;Initial Catalog=Carespot;User ID=carespot;Password=Test1234;Encrypt=False;TrustServerCertificate=True");
+
+        public List<Vrijwilliger> RetrieveAll()
+        {
+            var returnList = new List<Vrijwilliger>();
+
+            try
+            {
+                _con.Open();
+                var cmdString = "SELECT * FROM Vrijwilliger";
+                var command = new SqlCommand(cmdString, _con);
+                var reader = command.ExecuteReader();               
+         
+            }
+            catch(SqlException)
+            {
+               //
+            }
+            finally
+            {
+                _con.Close();
+            }
+            return returnList;
+        }
+
+        public void CreateVrijwilliger(Vrijwilliger v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vrijwilliger RetrieveVrijwilliger(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVrijwilliger(Vrijwilliger v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteVrijwilliger(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Carespot/DAL/Interfaces/IVrijwilligerContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Carespot.Models;
+
+namespace Carespot.DAL.Interfaces
+{
+    public interface IVrijwilligerContext
+    {
+        List<Vrijwilliger> RetrieveAll();
+
+        void CreateVrijwilliger(Vrijwilliger v);
+
+        Vrijwilliger RetrieveVrijwilliger(int id);
+
+        void UpdateVrijwilliger(Vrijwilliger v);
+
+        void DeleteVrijwilliger(int id);
+    }
+}
