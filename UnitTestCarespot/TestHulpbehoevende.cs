f6a3df29c0fd9d405ba525b0e391de2253b607d3
lucschmeits <luc.06@live.nl>
2017-04-06 11:19:14 +0200

    hulp

--- UnitTestCarespot/TestHulpbehoevende.cs
+++ UnitTestCarespot/TestHulpbehoevende.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Carespot.DAL.Context;
 using Carespot.DAL.Repositorys;
+using Carespot.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestCarespot
@@ -28,6 +29,30 @@ namespace UnitTestCarespot
             var hulpList = repo.HulpbehoevendeList();
 
             Assert.AreEqual("Jan", hulpList[0].Naam);
+        }
+
+        [TestMethod]
+        public void TestMethodHulpverlenerVanHulpbehoevende()
+        {
+            var inf = new HulpbehoevendeSQLContext();
+            var repo = new HulpbehoevendeRepository(inf);
+
+            var hulpList = repo.HulpbehoevendeList();
+
+            Assert.AreEqual("Pietje", hulpList[0].Hulpverlener.Naam);
+        }
+
+        [TestMethod]
+        public void TestMethodUpdateHulpbehoevende()
+        {
+            var inf = new HulpbehoevendeSQLContext();
+            var repo = new HulpbehoevendeRepository(inf);
+
+            var hulpb = repo.HulpbehoevendeList()[0];
+            hulpb.Naam = "Jantje";
+            repo.UpdateHulpbehoevende(hulpb);
+
+            Assert.AreEqual("Jantje", hulpb.Naam);
         }
     }
 }
