7ce51a0776aa1356c9b385fa2032749e5555aad6
PeterSmallenbroek <PeterSmallenbroek@users.noreply.github.com>
2017-04-06 10:04:42 +0200

    Beoordeling additions

---
+++ UnitTestCarespot/BeoordelingTest.cs
@@ -0,0 +1,30 @@
+﻿using Carespot.DAL.Context;
+using Carespot.DAL.Repositorys;
+using Carespot.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCarespot
+{
+    [TestClass]
+    public class BeoordelingTest
+    {
+        [TestMethod]
+        public void TestSelectBeoordeling()
+        {
+            var bsc = new BeoordelingSQLContext();
+            var br = new BeoordelingRepository(bsc);
+
+            var b = br.RetrieveBeoordeling(1);
+        }
+
+        [TestMethod]
+        public void TestCreateBeoordeling()
+        {
+            var bsc = new BeoordelingSQLContext();
+            var br = new BeoordelingRepository(bsc);
+
+            var b = new Beoordeling("Testopmerking", 7, 2, 4);
+            br.Create(b);
+        }
+    }
+}
