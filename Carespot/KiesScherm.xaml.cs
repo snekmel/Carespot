068856cc764cfc4ef176f42feb13b1b73b441bd4
LanceGils <lance.vangils@student.fontys.nl>
2017-04-13 13:11:06 +0200

    Testscherm inc knoppen

--- Carespot/KiesScherm.xaml.cs
+++ Carespot/KiesScherm.xaml.cs
@@ -26,9 +26,87 @@ namespace Carespot
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Inlogscherm inlog = new Inlogscherm();
-            inlog.Show();
+            Inlogscherm inlogscherm = new Inlogscherm();
+            inlogscherm.Show();
             
+        }
+
+        private void Keuzescherm_Click(object sender, RoutedEventArgs e)
+        {
+            Keuzescherm keuzescherm = new Keuzescherm();
+            keuzescherm.Show();
+        }
+
+        private void Aanmelden_Click(object sender, RoutedEventArgs e)
+        {
+            GebruikerAanmaken gebruikerAanmaken = new GebruikerAanmaken();
+            gebruikerAanmaken.Show();
+        }
+
+        private void Cliëntoverzicht_Click(object sender, RoutedEventArgs e)
+        {
+           CliëntOverzicht cliëntOverzicht = new CliëntOverzicht();
+           cliëntOverzicht.Show();
+        }
+
+        private void Hulpvraag_Click(object sender, RoutedEventArgs e)
+        {
+            Hulpvraagxaml hulpvraag = new Hulpvraagxaml();
+            hulpvraag.Show();
+        }
+
+        private void VrijwilligerOverzicht_Click(object sender, RoutedEventArgs e)
+        {
+            VrijwilligerHoofdscherm vrijwilligerHoofdscherm = new VrijwilligerHoofdscherm();
+            vrijwilligerHoofdscherm.Show();
+        }
+
+        private void OpdrachtAannemen_Click(object sender, RoutedEventArgs e)
+        {
+            VrijwilligerOpdrachtAannemen vrijwilligerOpdrachtAannemen = new VrijwilligerOpdrachtAannemen();
+            vrijwilligerOpdrachtAannemen.Show();
+        }
+
+        private void OpdrachtScherm_Click(object sender, RoutedEventArgs e)
+        {
+            Opdracht opdracht = new Opdracht();
+            opdracht.Show();
+        }
+
+        private void HulpverlenerToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            Carespot.HulpverlenerToevoegen hulpverlenerToevoegen = new HulpverlenerToevoegen();
+            hulpverlenerToevoegen.Show();
+        }
+
+        private void GebruikerBeheer_Click(object sender, RoutedEventArgs e)
+        {
+            Carespot.GebruikerBeheer gebruikerBeheer = new GebruikerBeheer();
+            gebruikerBeheer.Show();
+        }
+
+        private void VerwijderBevestiging_Click(object sender, RoutedEventArgs e)
+        {
+            Carespot.VerwijderBevestiging verwijderBevestiging = new VerwijderBevestiging();
+            verwijderBevestiging.Show();
+        }
+
+        private void BeheerderScherm_Click(object sender, RoutedEventArgs e)
+        {
+            Beheerderscherm beheerderscherm = new Beheerderscherm();
+            beheerderscherm.Show();
+        }
+
+        private void RecensieScherm_Click(object sender, RoutedEventArgs e)
+        {
+            Recensie recensie = new Recensie();
+            recensie.Show();
+        }
+
+        private void HulpverlenerOverzicht_Click(object sender, RoutedEventArgs e)
+        {
+            HulpverlenerHoofdscherm hulpverlenerHoofdscherm = new HulpverlenerHoofdscherm();
+            hulpverlenerHoofdscherm.Show();
         }
     }
 }
