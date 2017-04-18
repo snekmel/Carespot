c0493fdb45f544485b55821f9ae6895b902acf83
Sjoerd Lemkens <sjoerdlemkens@hotmail.com>
2017-04-18 12:08:14 +0200

    Verdere uitwerking reactie

--- Carespot/DAL/Context/ReactieSQLContext.cs
+++ Carespot/DAL/Context/ReactieSQLContext.cs
@@ -25,7 +25,7 @@ namespace Carespot.DAL.Context
                 SqlCommand cmd = new SqlCommand();
 
                 cmd.CommandText =
-                    "INSERT INTO Reactie (vrijwiligerid, hulpopdrachtid, bericht) VALUES ("+ vrijwillegerid +", "+ hulpaanvraagid +", '"+ bericht +"');";
+                    "INSERT INTO Reactie (vrijwilligerid, hulpopdrachtid, bericht) VALUES ("+ vrijwillegerid +", "+ hulpaanvraagid +", '"+ bericht +"');";
                 cmd.CommandType = CommandType.Text;
                 cmd.Connection = connection;
 
@@ -54,7 +54,7 @@ namespace Carespot.DAL.Context
                 SqlCommand cmd = new SqlCommand();
                 SqlDataReader reader;
 
-                cmd.CommandText = "SELECT * FROM Reactie WHERE =" + hulpopdrachtid + ";";
+                cmd.CommandText = "SELECT * FROM Reactie WHERE hulpopdrachtid =" + hulpopdrachtid + ";";
                 cmd.CommandType = CommandType.Text;
                 cmd.Connection = connection;
 
