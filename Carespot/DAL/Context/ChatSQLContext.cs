c83e00ae8faaf48a036250f939b077d4c737dfaa
PeterSmallenbroek <PeterSmallenbroek@users.noreply.github.com>
2017-04-23 14:33:05 +0200

    Chat exception/parameter

--- Carespot/DAL/Context/ChatSQLContext.cs
+++ Carespot/DAL/Context/ChatSQLContext.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Carespot.DAL.Interfaces;
 using Carespot.Models;
 
@@ -11,45 +8,81 @@ namespace Carespot.DAL.Context
 {
     public class ChatSQLContext : IChatContext
     {
-        private SqlConnection _con = new SqlConnection("Data Source=WIN-SRV-WEB.fhict.local;Initial Catalog=Carespot;User ID=carespot;Password=Test1234;Encrypt=False;TrustServerCertificate=True");
+        private readonly SqlConnection _con = new SqlConnection("Data Source=WIN-SRV-WEB.fhict.local;Initial Catalog=Carespot;User ID=carespot;Password=Test1234;Encrypt=False;TrustServerCertificate=True");
 
-        void IChatContext.CreateChatBericht(DateTime tijd, string bericht, int gebruikerId, int hulpopdrachtId)
+        public void CreateChatBericht(DateTime tijd, string bericht, int gebruikerId, int hulpopdrachtId)
         {
-            _con.Open();
-            string cmdString = "INSERT INTO ChatBericht (datum, bericht, gebruikerId, hulpopdrachtId) VALUES (@datum, '" + bericht + "', '" + gebruikerId + "', '" + hulpopdrachtId + "')";
-            SqlCommand command = new SqlCommand(cmdString, _con);
-            command.Parameters.AddWithValue("@datum", DateTime.Now);
-            command.ExecuteNonQuery();
-            _con.Close();
-        }
-
-        void IChatContext.DeleteChatBericht(int id)
-        {
-            _con.Open();
-            var cmdString = "DELETE FROM ChatBericht WHERE id = '" + id + "'";
-            var command = new SqlCommand(cmdString, _con);
-            command.ExecuteNonQuery();
-            _con.Close();
-        }
-
-        List<ChatBericht> IChatContext.RetrieveAllChatBerichtenByOpdracht(int id)
-        {
-            _con.Open(); string cmdString = "SELECT * FROM ChatBericht WHERE hulpopdrachtId = '" + id + "'";
-            SqlCommand command = new SqlCommand(cmdString, _con);
-            SqlDataReader reader = command.ExecuteReader();
-            List<ChatBericht> ReturnList = new List<ChatBericht>();
-
-            while (reader.Read())
+            try
             {
-                var c = new ChatBericht(reader.GetDateTime(1), reader.GetString(2), reader.GetInt32(3), reader.GetInt32(4));
-                c.Id = reader.GetInt32(0);
-
-                ReturnList.Add(c);
+                using (_con)
+                {
+                    _con.Open();
+                    var cmdString = "INSERT INTO ChatBericht (datum, bericht, gebruikerId, hulpopdrachtId) VALUES (@datum, @bericht, @gebruikerId, @hulpopdrachtId)";
+                    var command = new SqlCommand(cmdString, _con);
+                    command.Parameters.AddWithValue("@datum", DateTime.Now);
+                    command.Parameters.AddWithValue("@bericht", bericht);
+                    command.Parameters.AddWithValue("@gebruikerId", gebruikerId);
+                    command.Parameters.AddWithValue("@hulpopdrachtId", hulpopdrachtId);
+                    command.ExecuteNonQuery();
+                    _con.Close();
+                }
             }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Er ging iets mis bij het ophalen van de gegevens", ex);
+            }
+        }
 
-            _con.Close();
-            reader.Close();
-            return ReturnList;
+        public void DeleteChatBericht(int id)
+        {
+            try
+            {
+                using (_con)
+                {
+                    _con.Open();
+                    var cmdString = "DELETE FROM ChatBericht WHERE id = @id";
+                    var command = new SqlCommand(cmdString, _con);
+                    command.Parameters.AddWithValue("@id", id);
+                    command.ExecuteNonQuery();
+                    _con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Er ging iets mis bij het ophalen van de gegevens", ex);
+            }
+        }
+
+        public List<ChatBericht> RetrieveAllChatBerichtenByOpdracht(int id)
+        {
+            try
+            {
+                using (_con)
+                {
+                    _con.Open();
+                    var cmdString = "SELECT * FROM ChatBericht WHERE hulpopdrachtId = @id";
+                    var command = new SqlCommand(cmdString, _con);
+                    command.Parameters.AddWithValue("@id", id);
+                    var reader = command.ExecuteReader();
+                    var ReturnList = new List<ChatBericht>();
+
+                    while (reader.Read())
+                    {
+                        var c = new ChatBericht(reader.GetDateTime(1), reader.GetString(2), reader.GetInt32(3), reader.GetInt32(4));
+                        c.Id = reader.GetInt32(0);
+
+                        ReturnList.Add(c);
+                    }
+
+                    _con.Close();
+                    reader.Close();
+                    return ReturnList;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Er ging iets mis bij het ophalen van de gegevens", ex);
+            }
         }
     }
 }
